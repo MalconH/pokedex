6e9ff49f81925711afcb748199676c10e5d64a7f
Malcon <malcon.heltner@gmail.com>
2023-12-13 15:40:16 -0300

    Arregla problemas de estilo
    Al introducir ESLint, se cambia la indentacion de 4 a 2 espacios y
    se usan comillas simples en vez de dobles.

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,225 +1,222 @@
+describe('Testeando Pokedex', () => {
+  const POKEMONES_POR_PAGINA = 20;
+  beforeEach(() => {
+    const URL = '127.0.0.1:8080';
 
-describe("Testeando Pokedex", () => {
-    const POKEMONES_POR_PAGINA = 20;
-    beforeEach(() => {
-        const URL = "127.0.0.1:8080";
+    for (let i = 1; i <= POKEMONES_POR_PAGINA * 2; i++) {
+      cy.fixture('pokemones.json').then((fixturePokemones) => {
+        const datosPokemonActual = { body: fixturePokemones[i - 1] };
+        cy.intercept(`https://pokeapi.co/api/v2/pokemon/${i}`, datosPokemonActual).as(`pokemon${i}`);
+      });
+    }
 
-        for (let i = 1; i <= POKEMONES_POR_PAGINA * 2; i++) {
-            cy.fixture("pokemones.json").then((fixturePokemones) => {
-                const datosPokemonActual = { body: fixturePokemones[i - 1] };
-                cy.intercept(`https://pokeapi.co/api/v2/pokemon/${i}`, datosPokemonActual).as(`pokemon${i}`);
-            });
+    // Puedo stubear un intercept con un objeto.
+    // Agarro el fixture, agarro el objeto en la posi que me interesa
+    // y creo un objeto con eso, lo stubeo
 
-        }
+    cy.visit(URL);
+  });
 
+  it('Carga correctamente las carta pokemon', () => {
+    cy.get('.carta').should('have.length', POKEMONES_POR_PAGINA);
 
-        // Puedo stubear un intercept con un objeto. 
-        // Agarro el fixture, agarro el objeto en la posi que me interesa
-        // y creo un objeto con eso, lo stubeo
-
-        cy.visit(URL);
-
+    cy.get('.carta').each((carta) => {
+      cy.wrap(carta)
+        .find('.card-title')
+        .should('be.visible');
+      cy.wrap(carta)
+        .find('.card-subtitle')
+        .should('be.visible');
+      cy.wrap(carta)
+        .find('.card-img-top')
+        .should('have.attr', 'src');
     });
+  });
 
-    it("Carga correctamente las carta pokemon", () => {
+  it('Carga correctamente las cartas para cada página', () => {
+    verificarCartasEnPagina(1);
 
-        cy.get(".carta").should("have.length", POKEMONES_POR_PAGINA);
+    cy.get("nav [data-indice='2']").click();
 
-        cy.get(".carta").each((carta) => {
-            cy.wrap(carta)
-                .find(".card-title")
-                .should("be.visible");
-            cy.wrap(carta)
-                .find(".card-subtitle")
-                .should("be.visible");
-            cy.wrap(carta)
-                .find(".card-img-top")
-                .should("have.attr", "src");
-        });
-    });
+    verificarCartasEnPagina(2);
+  });
 
-    it("Carga correctamente las cartas para cada página", () => {
-        verificarCartasEnPagina(1);
+  it('Carga correctamente los datos del modal', () => {
+    const ID_POKEMON_1 = 1;
+    const ID_POKEMON_2 = 8;
 
-        cy.get("nav [data-indice='2']").click();
+    cy.get(`[data-pokemon-id = '${ID_POKEMON_1}']`).click();
+    cy.get('.modal').should('be.visible');
+    comprobarDatosModal(ID_POKEMON_1);
 
-        verificarCartasEnPagina(2);
-    });
+    cy.wait(500);
+    cy.get('.modal .btn-close').click(); // cierra el modal;
+    cy.get('.modal')
+      .should('not.be.visible');
 
-    it("Carga correctamente los datos del modal", () => {
-        const ID_POKEMON_1 = 1;
-        const ID_POKEMON_2 = 8;
+    cy.get(`[data-pokemon-id = '${ID_POKEMON_2}']`).click();
+    comprobarDatosModal(ID_POKEMON_2);
+  });
 
-        cy.get(`[data-pokemon-id = '${ID_POKEMON_1}']`).click();
-        cy.get(".modal").should("be.visible");
-        comprobarDatosModal(ID_POKEMON_1);
+  it.only('El paginador funciona correctamente', () => {
+    cy.get('nav .page-link.active').as('paginaSeleccionada')
+      .should('have.text', '1');
+    cy.get("[data-indice='anterior']")
+      .should('have.class', 'disabled');
 
-        cy.wait(500);
-        cy.get(".modal .btn-close").click(); // cierra el modal;
-        cy.get(".modal")
-            .should("not.be.visible");
+    cy.get("[data-indice='2']")
+      .should('have.text', '2')
+      .click();
 
-        cy.get(`[data-pokemon-id = '${ID_POKEMON_2}']`).click();
-        comprobarDatosModal(ID_POKEMON_2);
-    });
+    cy.get('@paginaSeleccionada')
+      .should('have.text', '2');
 
-    it.only("El paginador funciona correctamente", () => {
-        cy.get("nav .page-link.active").as("paginaSeleccionada")
-            .should("have.text", "1");
-        cy.get("[data-indice='anterior']")
-            .should("have.class", "disabled");
+    cy.get("[data-indice='3']")
+      .should('have.text', '3')
+      .click();
 
-        cy.get("[data-indice='2']")
-            .should("have.text", "2")
-            .click();
+    cy.get('@paginaSeleccionada')
+      .should('have.text', '3');
 
-        cy.get("@paginaSeleccionada")
-            .should("have.text", "2");
+    cy.get("[data-indice='siguiente']")
+      .should('have.text', 'Siguiente')
+      .click();
 
-        cy.get("[data-indice='3']")
-            .should("have.text", "3")
-            .click();
+    cy.get('@paginaSeleccionada')
+      .should('have.text', '4');
 
-        cy.get("@paginaSeleccionada")
-            .should("have.text", "3");
+    cy.get("[data-indice='anterior']")
+      .should('have.text', 'Anterior')
+      .click().click();
 
-        cy.get("[data-indice='siguiente']")
-            .should("have.text", "Siguiente")
-            .click();
-
-        cy.get("@paginaSeleccionada")
-            .should("have.text", "4");
-
-        cy.get("[data-indice='anterior']")
-            .should("have.text", "Anterior")
-            .click().click();
-
-        cy.get("@paginaSeleccionada")
-            .should("have.text", "2");
-    });
+    cy.get('@paginaSeleccionada')
+      .should('have.text', '2');
+  });
 });
 
 function extraerDatosPokemon(pokemon) {
-    const {
-        id: id,
-        name: nombre,
-        sprites: { other: { "official-artwork": {
-            front_default: urlImagen } }
-        },
-        types: tiposSinMapear,
-        weight: pesoEnHG,
-        height: alturaEnDM,
-        stats: estadisticasSinMapear
-    } = pokemon;
+  const {
+    id,
+    name: nombre,
+    sprites: {
+      other: {
+        'official-artwork': { front_default: urlImagen },
+      },
+    },
+    types: tiposSinMapear,
+    weight: pesoEnHG,
+    height: alturaEnDM,
+    stats: estadisticasSinMapear,
+  } = pokemon;
 
-    const tipos = tiposSinMapear.map((item) => item.type.name);
-    const estadisticas = {};
-    Object.keys(estadisticasSinMapear).forEach((indiceEstadistica) => {
-        estadisticas[estadisticasSinMapear[indiceEstadistica].stat.name] = estadisticasSinMapear[indiceEstadistica].base_stat;
-    });
+  const tipos = tiposSinMapear.map((item) => item.type.name);
+  const estadisticas = {};
+  Object.keys(estadisticasSinMapear).forEach((indiceEstadistica) => {
+    estadisticas[estadisticasSinMapear[indiceEstadistica].stat.name] = estadisticasSinMapear[indiceEstadistica].base_stat;
+  });
 
-    return {
-        id: id,
-        nombre: nombre,
-        urlImagen: urlImagen,
-        tipos: tipos,
-        peso: pesoEnHG,
-        altura: alturaEnDM,
-        estadisticas: estadisticas
-    };
+  return {
+    id,
+    nombre,
+    urlImagen,
+    tipos,
+    peso: pesoEnHG,
+    altura: alturaEnDM,
+    estadisticas,
+  };
 }
 
 function verificarCartasEnPagina(nroPagina) {
-    const POKEMONES_POR_PAGINA = 20;
-    const offsetPokemon = (nroPagina - 1) * POKEMONES_POR_PAGINA;
+  const POKEMONES_POR_PAGINA = 20;
+  const offsetPokemon = (nroPagina - 1) * POKEMONES_POR_PAGINA;
 
-    cy.fixture("pokemones.json").then((pokemones) => {
-        for (let i = 0; i < 20; i++) {
-            const {
-                id: idPokemon,
-                nombre: nombrePokemon,
-                urlImagen: urlImagenPokemon,
-                tipos: tiposPokemon
-            } = extraerDatosPokemon(pokemones[i + offsetPokemon]);
+  cy.fixture('pokemones.json').then((pokemones) => {
+    for (let i = 0; i < 20; i++) {
+      const {
+        id: idPokemon,
+        nombre: nombrePokemon,
+        urlImagen: urlImagenPokemon,
+        tipos: tiposPokemon,
+      } = extraerDatosPokemon(pokemones[i + offsetPokemon]);
 
-            cy.get(".carta")
-                .eq(i)
-                .as("cartaActual");
+      cy.get('.carta')
+        .eq(i)
+        .as('cartaActual');
 
-            cy.get("@cartaActual")
-                .should("have.attr", "data-pokemon-id", idPokemon);
+      cy.get('@cartaActual')
+        .should('have.attr', 'data-pokemon-id', idPokemon);
 
-            cy.get("@cartaActual")
-                .find("img")
-                .should("have.attr", "src", urlImagenPokemon)
-                .should("have.attr", "alt", `Arte original del pokemon ${nombrePokemon}`);
+      cy.get('@cartaActual')
+        .find('img')
+        .should('have.attr', 'src', urlImagenPokemon)
+        .should('have.attr', 'alt', `Arte original del pokemon ${nombrePokemon}`);
 
-            cy.wrap(tiposPokemon).each((tipoPokemon) => {
-                cy.get("@cartaActual")
-                    .find(".card-subtitle")
-                    .should("contain.text", tipoPokemon);
-            });
-        }
-    });
+      cy.wrap(tiposPokemon).each((tipoPokemon) => {
+        cy.get('@cartaActual')
+          .find('.card-subtitle')
+          .should('contain.text', tipoPokemon);
+      });
+    }
+  });
 }
 
 function comprobarDatosModal(idPokemon) {
-    cy.fixture("pokemones.json").then((pokemones) => {
-        const datosPokemon = extraerDatosPokemon(pokemones[idPokemon - 1]);
+  cy.fixture('pokemones.json').then((pokemones) => {
+    const datosPokemon = extraerDatosPokemon(pokemones[idPokemon - 1]);
 
-        cy.get(".modal")
-            .should("be.visible");
+    cy.get('.modal')
+      .should('be.visible');
 
-        cy.get(".modal")
-            .find(".pokemon-imagen")
-            .should("have.attr", "src", datosPokemon.urlImagen);
+    cy.get('.modal')
+      .find('.pokemon-imagen')
+      .should('have.attr', 'src', datosPokemon.urlImagen);
 
-        cy.get(".modal")
-            .find(".pokemon-peso")
-            .should("contain.text", hectogramosAKG(datosPokemon.peso));
+    cy.get('.modal')
+      .find('.pokemon-peso')
+      .should('contain.text', hectogramosAKG(datosPokemon.peso));
 
-        cy.get(".modal")
-            .find(".pokemon-altura")
-            .should("contain.text", decimetrosAM(datosPokemon.altura));
+    cy.get('.modal')
+      .find('.pokemon-altura')
+      .should('contain.text', decimetrosAM(datosPokemon.altura));
 
-        cy.wrap(datosPokemon.tipos).each((tipo) => {
-            cy.get(".modal")
-                .find(`.pokemon-tipo.${tipo}`)
-                .should("contain.text", tipo);
-        });
-
-        cy.get(".modal")
-            .find(".hp")
-            .should("contain.text", datosPokemon.estadisticas.hp);
-
-        cy.get(".modal")
-            .find(".attack")
-            .should("contain.text", datosPokemon.estadisticas.attack);
-
-        cy.get(".modal")
-            .find(".defense")
-            .should("contain.text", datosPokemon.estadisticas.defense);
-
-        cy.get(".modal")
-            .find(".speed")
-            .should("contain.text", datosPokemon.estadisticas.speed);
-
-        cy.get(".modal")
-            .find(".special-attack")
-            .should("contain.text", datosPokemon.estadisticas["special-attack"]);
-
-        cy.get(".modal")
-            .find(".special-defense")
-            .should("contain.text", datosPokemon.estadisticas["special-defense"]);
+    cy.wrap(datosPokemon.tipos).each((tipo) => {
+      cy.get('.modal')
+        .find(`.pokemon-tipo.${tipo}`)
+        .should('contain.text', tipo);
     });
+
+    cy.get('.modal')
+      .find('.hp')
+      .should('contain.text', datosPokemon.estadisticas.hp);
+
+    cy.get('.modal')
+      .find('.attack')
+      .should('contain.text', datosPokemon.estadisticas.attack);
+
+    cy.get('.modal')
+      .find('.defense')
+      .should('contain.text', datosPokemon.estadisticas.defense);
+
+    cy.get('.modal')
+      .find('.speed')
+      .should('contain.text', datosPokemon.estadisticas.speed);
+
+    cy.get('.modal')
+      .find('.special-attack')
+      .should('contain.text', datosPokemon.estadisticas['special-attack']);
+
+    cy.get('.modal')
+      .find('.special-defense')
+      .should('contain.text', datosPokemon.estadisticas['special-defense']);
+  });
 }
 
 function hectogramosAKG(hectogramo) {
-    return hectogramo / 10;
+  return hectogramo / 10;
 }
 
 function decimetrosAM(decimetros) {
-    return decimetros / 10;
+  return decimetros / 10;
 }
 
 /* function comprobarDatosCartaPokemon($carta, id, nombre, tipos) {
--- src/index.js
+++ src/index.js
@@ -1,67 +1,69 @@
 async function cargarPokemones(offset, cantidad) {
-    for (let i = 1; i <= cantidad; i++) {
-        await obtenerPokemon(i, offset).then(respuestaPokemon => {
-            const {
-                id: id,
-                name: nombrePokemon,
-                types: tipos,
-                sprites: { other: { "official-artwork": {
-                    front_default: urlPokemon } }
-                }
-            } = respuestaPokemon;
+  for (let i = 1; i <= cantidad; i++) {
+    await obtenerPokemon(i, offset).then((respuestaPokemon) => {
+      const {
+        id,
+        name: nombrePokemon,
+        types: tipos,
+        sprites: {
+          other: {
+            'official-artwork': { front_default: urlPokemon },
+          },
+        },
+      } = respuestaPokemon;
 
-            crearCartaPokemon(
-                nombrePokemon,
-                urlPokemon,
-                tipos.map((item) => item.type.name),
-                id
-            );
-        });
-    }
+      crearCartaPokemon(
+        nombrePokemon,
+        urlPokemon,
+        tipos.map((item) => item.type.name),
+        id,
+      );
+    });
+  }
 }
 
 function obtenerPokemon(id, offset = 0) {
-    const URL_BASE = "https://pokeapi.co/api/v2/";
+  const URL_BASE = 'https://pokeapi.co/api/v2/';
 
-    return fetch(`${URL_BASE}pokemon/${id + offset}`)
-        .then(respuesta => respuesta.json());
+  return fetch(`${URL_BASE}pokemon/${id + offset}`)
+    .then((respuesta) => respuesta.json());
 }
 
 function crearCartaPokemon(nombre, urlImagen, tipos, id) {
-    const $carta = crearCarta();
-    const $contenedorCarta = document.createElement("div");
-    $contenedorCarta.className = "col";
+  const $carta = crearCarta();
+  const $contenedorCarta = document.createElement('div');
+  $contenedorCarta.className = 'col';
 
-    // Selecciono los elementos para los datos del pokemon
-    const $contenedorCartas = document.querySelector(".cartas .row");
-    const $nombrePokemon = $carta.querySelector("h3.card-title");
-    const $imagenPokemon = $carta.querySelector("img.card-img-top");
-    const $tiposPokemon = $carta.querySelectorAll(".card-subtitle .badge");
+  // Selecciono los elementos para los datos del pokemon
+  const $contenedorCartas = document.querySelector('.cartas .row');
+  const $nombrePokemon = $carta.querySelector('h3.card-title');
+  const $imagenPokemon = $carta.querySelector('img.card-img-top');
+  const $tiposPokemon = $carta.querySelectorAll('.card-subtitle .badge');
 
-    $carta.setAttribute("data-pokemon-id", id);
-    $nombrePokemon.textContent = mayusculaEnPrimaLetra(nombre);
-    $imagenPokemon.src = urlImagen;
-    $imagenPokemon.alt = `Arte original del pokemon ${nombre}`;
+  $carta.setAttribute('data-pokemon-id', id);
+  $nombrePokemon.textContent = mayusculaEnPrimaLetra(nombre);
+  $imagenPokemon.src = urlImagen;
+  $imagenPokemon.alt = `Arte original del pokemon ${nombre}`;
 
-    if (tipos.length > 1) {
-        tipos.forEach((tipo, i) => {
-            $tiposPokemon[i].textContent = tipo;
-            $tiposPokemon[i].classList.remove("bg-secondary");
-            $tiposPokemon[i].classList.add(tipo);
-        });
-    } else {
-        $tiposPokemon[0].textContent = tipos;
-        $tiposPokemon[0].classList.remove("bg-secondary");
-        $tiposPokemon[0].classList.add(tipos);
-        $tiposPokemon[1].remove();
-    }
+  if (tipos.length > 1) {
+    tipos.forEach((tipo, i) => {
+      $tiposPokemon[i].textContent = tipo;
+      $tiposPokemon[i].classList.remove('bg-secondary');
+      $tiposPokemon[i].classList.add(tipo);
+    });
+  } else {
+    $tiposPokemon[0].textContent = tipos;
+    $tiposPokemon[0].classList.remove('bg-secondary');
+    $tiposPokemon[0].classList.add(tipos);
+    $tiposPokemon[1].remove();
+  }
 
-    $contenedorCarta.appendChild($carta);
-    $contenedorCartas.appendChild($contenedorCarta);
+  $contenedorCarta.appendChild($carta);
+  $contenedorCartas.appendChild($contenedorCarta);
 }
 
 function crearCarta() {
-    const htmlCarta = `
+  const htmlCarta = `
           <img src="#" alt="..." class="card-img-top">
            <div class="card-body">
                <h3 class="card-title">Lorem, ipsum dolor.</h3>
@@ -71,141 +73,137 @@ function crearCarta() {
                 </h4>
             </div>`;
 
-    const $carta = document.createElement("div");
-    $carta.innerHTML = htmlCarta;
-    $carta.className = "carta card shadow-sm";
+  const $carta = document.createElement('div');
+  $carta.innerHTML = htmlCarta;
+  $carta.className = 'carta card shadow-sm';
 
-    return $carta;
+  return $carta;
 }
 
 function mayusculaEnPrimaLetra(palabra) {
-    const primeraLetra = palabra.slice(0, 1).toUpperCase();
-    return primeraLetra + palabra.slice(1, palabra.length);
+  const primeraLetra = palabra.slice(0, 1).toUpperCase();
+  return primeraLetra + palabra.slice(1, palabra.length);
 }
 
-
 function cambiarPagina(pagina) {
-    borrarPokemonesAnteriores();
-    const paginaActual = pagina;
-    const POKEMONES_POR_PAGINA = 20;
-    const offset = POKEMONES_POR_PAGINA * (paginaActual - 1);
+  borrarPokemonesAnteriores();
+  const paginaActual = pagina;
+  const POKEMONES_POR_PAGINA = 20;
+  const offset = POKEMONES_POR_PAGINA * (paginaActual - 1);
 
-    cargarPokemones(offset, POKEMONES_POR_PAGINA);
+  cargarPokemones(offset, POKEMONES_POR_PAGINA);
 }
 
 function borrarPokemonesAnteriores() {
-    document.querySelectorAll(".col").forEach((carta) => {
-        carta.remove();
-    });
+  document.querySelectorAll('.col').forEach((carta) => {
+    carta.remove();
+  });
 }
 
 // Modal
-document.querySelector(".cartas").onclick = function (e) {
-    const idPokemon = Number(e.target.getAttribute("data-pokemon-id"));
-    if (idPokemon !== 0) {
-        mostrarModalPokemon(idPokemon);
-    }
+document.querySelector('.cartas').onclick = function (e) {
+  const idPokemon = Number(e.target.getAttribute('data-pokemon-id'));
+  if (idPokemon !== 0) {
+    mostrarModalPokemon(idPokemon);
+  }
 };
 
 function mostrarModalPokemon(idPokemon) {
-    const modalPokemon = new bootstrap.Modal(document.querySelector("#modal-pokemon"), {});
-    const $modal = modalPokemon["_element"];
+  const modalPokemon = new bootstrap.Modal(document.querySelector('#modal-pokemon'), {});
+  const $modal = modalPokemon._element;
 
-    obtenerPokemon(idPokemon).then(pokemon => {
-        // Desempaco el JSON a constantes:
+  obtenerPokemon(idPokemon).then((pokemon) => {
+    // Desempaco el JSON a constantes:
 
-        const {
-            name: nombre,
-            sprites: { other: { "official-artwork": {
-                front_default: urlImagen } }
-            },
-            types: tipos,
-            weight: pesoEnHectogramos,
-            height: alturaEnDecimetros,
-            stats: estadisticas
-        } = pokemon;
+    const {
+      name: nombre,
+      sprites: {
+        other: {
+          'official-artwork': { front_default: urlImagen },
+        },
+      },
+      types: tipos,
+      weight: pesoEnHectogramos,
+      height: alturaEnDecimetros,
+      stats: estadisticas,
+    } = pokemon;
 
-        cargarDatosModal(
-            mayusculaEnPrimaLetra(nombre),
-            urlImagen,
-            tipos.map((tipo => {
-                return tipo.type.name;
-            })),
-            hectogramosAKG(pesoEnHectogramos),
-            decimetrosAM(alturaEnDecimetros),
-            estadisticas.map(estadistica => {
-                const nuevaEstadistica = {};
+    cargarDatosModal(
+      mayusculaEnPrimaLetra(nombre),
+      urlImagen,
+      tipos.map(((tipo) => tipo.type.name)),
+      hectogramosAKG(pesoEnHectogramos),
+      decimetrosAM(alturaEnDecimetros),
+      estadisticas.map((estadistica) => {
+        const nuevaEstadistica = {};
 
-                // El key:value es nombre_estadistica:valor_estadistica, p ej: hp: 50
-                nuevaEstadistica[estadistica.stat.name] = estadistica.base_stat;
-                return nuevaEstadistica;
-            }).flat(),
-            $modal
-        );
-    });
+        // El key:value es nombre_estadistica:valor_estadistica, p ej: hp: 50
+        nuevaEstadistica[estadistica.stat.name] = estadistica.base_stat;
+        return nuevaEstadistica;
+      }).flat(),
+      $modal,
+    );
+  });
 
-    modalPokemon.show();
+  modalPokemon.show();
 }
 
 function cargarDatosModal(nombre, imagen, tipos, pesoKG, alturaMetros, estadisticas, modal) {
-    const $titulo = modal.querySelector(".modal-title");
-    const $cuerpo = modal.querySelector(".modal-body");
-    const $tiposPokemon = modal.querySelectorAll(".pokemon-tipo");
-    const $imagen = document.querySelector(".pokemon-imagen");
+  const $titulo = modal.querySelector('.modal-title');
+  const $cuerpo = modal.querySelector('.modal-body');
+  const $tiposPokemon = modal.querySelectorAll('.pokemon-tipo');
+  const $imagen = document.querySelector('.pokemon-imagen');
 
-    // Carga imagen
-    $imagen.src = imagen;
-    $imagen.alt = nombre;
+  // Carga imagen
+  $imagen.src = imagen;
+  $imagen.alt = nombre;
 
-    // Carga peso y altura
-    document.querySelector(".pokemon-peso").textContent = `${pesoKG}kg`;
-    document.querySelector(".pokemon-altura").textContent = `${alturaMetros}m`;
+  // Carga peso y altura
+  document.querySelector('.pokemon-peso').textContent = `${pesoKG}kg`;
+  document.querySelector('.pokemon-altura').textContent = `${alturaMetros}m`;
 
-    // Carga estadisticas
-    estadisticas.forEach((estadistica => {
-        const nombreEstadistica = Object.keys(estadistica);
-        const valorEstadistica = estadistica[nombreEstadistica];
+  // Carga estadisticas
+  estadisticas.forEach(((estadistica) => {
+    const nombreEstadistica = Object.keys(estadistica);
+    const valorEstadistica = estadistica[nombreEstadistica];
 
-        document.querySelector(`.${nombreEstadistica}-valor`).textContent = valorEstadistica;
-    }));
+    document.querySelector(`.${nombreEstadistica}-valor`).textContent = valorEstadistica;
+  }));
 
-    // Carga los tipos
-    resetearClasesTiposPokemonModal();
-    if (tipos.length > 1) {
-        tipos.forEach((tipo, i) => {
-            $tiposPokemon[i].textContent = tipo;
-            $tiposPokemon[i].classList.remove("bg-secondary");
-            $tiposPokemon[i].classList.add(tipo);
-        });
-    } else {
-        $tiposPokemon[0].textContent = tipos;
-        $tiposPokemon[0].classList.remove("bg-secondary");
-        $tiposPokemon[0].classList.add(tipos);
-        $tiposPokemon[1].classList.add("d-none");
-    }
+  // Carga los tipos
+  resetearClasesTiposPokemonModal();
+  if (tipos.length > 1) {
+    tipos.forEach((tipo, i) => {
+      $tiposPokemon[i].textContent = tipo;
+      $tiposPokemon[i].classList.remove('bg-secondary');
+      $tiposPokemon[i].classList.add(tipo);
+    });
+  } else {
+    $tiposPokemon[0].textContent = tipos;
+    $tiposPokemon[0].classList.remove('bg-secondary');
+    $tiposPokemon[0].classList.add(tipos);
+    $tiposPokemon[1].classList.add('d-none');
+  }
 
-    $titulo.textContent = nombre;
-
-
-
+  $titulo.textContent = nombre;
 }
 
 function hectogramosAKG(hectogramo) {
-    return hectogramo / 10;
+  return hectogramo / 10;
 }
 
 function decimetrosAM(decimetros) {
-    return decimetros / 10;
+  return decimetros / 10;
 }
 
 function resetearClasesTiposPokemonModal() {
-    document.querySelectorAll(".pokemon-tipo").forEach((tipo) => {
-        tipo.className = "pokemon-tipo badge bg-secondary";
-    });
+  document.querySelectorAll('.pokemon-tipo').forEach((tipo) => {
+    tipo.className = 'pokemon-tipo badge bg-secondary';
+  });
 }
 
 function inicializar() {
-    cambiarPagina(1);
+  cambiarPagina(1);
 }
 
 inicializar();

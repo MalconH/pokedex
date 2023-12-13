d979e784701c56d2ecf71359909202e187aaed48
Malcon <malcon.heltner@gmail.com>
2023-12-13 17:07:11 -0300

    Refactoriza la manera de cargar pokemones
    Anteriormente, a través de un for loop se obtenía cada pokemon, y luego
    se creaban las cartas. Para que estas se crearan en orden, se usaba
    un `await` ante cada llamada a `obtenerPokemon()`.
    Dado que ESLint no permite awaits en loops (ver `no-await-in-loop`), lo
    que se hace es:
    
    1. Se precrean vacias las cartas porque queda mejor en la interfaz.
    2. En vez de usar `await`, se utiliza el indice actual del loop que se encarga
    de hacer las 20 llamadas a la API para los datos de cada pokemon se
    cargue en el orden correcto en la carta que corresponda.

--- src/index.js
+++ src/index.js
@@ -1,88 +1,56 @@
-async function cargarPokemones(offset, cantidad) {
-  for (let i = 1; i <= cantidad; i++) {
-    await obtenerPokemon(i, offset).then((respuestaPokemon) => {
-      const {
-        id,
-        name: nombrePokemon,
-        types: tipos,
-        sprites: {
-          other: {
-            'official-artwork': { front_default: urlPokemon },
-          },
-        },
-      } = respuestaPokemon;
-
-      crearCartaPokemon(
-        nombrePokemon,
-        urlPokemon,
-        tipos.map((item) => item.type.name),
-        id,
-      );
-    });
-  }
-}
-
-function obtenerPokemon(id, offset = 0) {
+function obtenerPokemon(id) {
   const URL_BASE = 'https://pokeapi.co/api/v2/';
 
-  return fetch(`${URL_BASE}pokemon/${id + offset}`)
+  return fetch(`${URL_BASE}pokemon/${id}`)
     .then((respuesta) => respuesta.json());
-}
-
-function crearCartaPokemon(nombre, urlImagen, tipos, id) {
-  const $carta = crearCarta();
-  const $contenedorCarta = document.createElement('div');
-  $contenedorCarta.className = 'col';
-
-  // Selecciono los elementos para los datos del pokemon
-  const $contenedorCartas = document.querySelector('.cartas .row');
-  const $nombrePokemon = $carta.querySelector('h3.card-title');
-  const $imagenPokemon = $carta.querySelector('img.card-img-top');
-  const $tiposPokemon = $carta.querySelectorAll('.card-subtitle .badge');
-
-  $carta.setAttribute('data-pokemon-id', id);
-  $nombrePokemon.textContent = mayusculaEnPrimaLetra(nombre);
-  $imagenPokemon.src = urlImagen;
-  $imagenPokemon.alt = `Arte original del pokemon ${nombre}`;
-
-  if (tipos.length > 1) {
-    tipos.forEach((tipo, i) => {
-      $tiposPokemon[i].textContent = tipo;
-      $tiposPokemon[i].classList.remove('bg-secondary');
-      $tiposPokemon[i].classList.add(tipo);
-    });
-  } else {
-    $tiposPokemon[0].textContent = tipos;
-    $tiposPokemon[0].classList.remove('bg-secondary');
-    $tiposPokemon[0].classList.add(tipos);
-    $tiposPokemon[1].remove();
-  }
-
-  $contenedorCarta.appendChild($carta);
-  $contenedorCartas.appendChild($contenedorCarta);
-}
-
-function crearCarta() {
-  const htmlCarta = `
-          <img src="#" alt="..." class="card-img-top">
-           <div class="card-body">
-               <h3 class="card-title">Lorem, ipsum dolor.</h3>
-                <h4 class="card-subtitle h6">
-                    <span class="badge bg-secondary"></span>
-                    <span class="badge bg-secondary"></span>
-                </h4>
-            </div>`;
-
-  const $carta = document.createElement('div');
-  $carta.innerHTML = htmlCarta;
-  $carta.className = 'carta card shadow-sm';
-
-  return $carta;
 }
 
 function mayusculaEnPrimaLetra(palabra) {
   const primeraLetra = palabra.slice(0, 1).toUpperCase();
   return primeraLetra + palabra.slice(1, palabra.length);
+}
+
+function crearCartasVacias(cantidad) {
+  for (let i = 0; i < cantidad; i++) {
+    const $carta = document.createElement('div');
+    const htmlCarta = `
+          <img src="#" alt="" class="card-img-top">
+           <div class="card-body">
+               <h3 class="card-title">Cargando...</h3>
+                <h4 class="card-subtitle h6">
+                    <span class="badge"></span>
+                    <span class="badge"></span>
+                </h4>
+            </div>`;
+    $carta.innerHTML = htmlCarta;
+    $carta.className = 'carta card shadow-sm';
+
+    const $contenedorCartas = document.querySelector('.cartas .row');
+    const $contenedorCarta = document.createElement('div');
+    $contenedorCarta.className = 'col';
+
+    $contenedorCarta.appendChild($carta);
+    $contenedorCartas.appendChild($contenedorCarta);
+  }
+}
+
+function cargarDatosPokemon(nombre, tipos, urlSprite, id, indice) {
+  const $carta = document.querySelectorAll('.carta')[indice];
+  const $imagen = $carta.querySelector('.card-img-top');
+  const $nombre = $carta.querySelector('.card-title');
+  const $tipos = $carta.querySelectorAll('.card-subtitle .badge');
+
+  $carta.setAttribute('data-pokemon-id', id);
+
+  $imagen.alt = `Arte oficial del Pokemon ${nombre}`;
+  $imagen.src = urlSprite;
+
+  $nombre.textContent = mayusculaEnPrimaLetra(nombre);
+
+  tipos.forEach((tipo, i) => {
+    $tipos[i].textContent = tipo;
+    $tipos[i].classList.add(tipo);
+  });
 }
 
 function cambiarPagina(pagina) {
@@ -91,7 +59,31 @@ function cambiarPagina(pagina) {
   const POKEMONES_POR_PAGINA = 20;
   const offset = POKEMONES_POR_PAGINA * (paginaActual - 1);
 
-  cargarPokemones(offset, POKEMONES_POR_PAGINA);
+  crearCartasVacias(20);
+
+  for (let i = 0; i < POKEMONES_POR_PAGINA; i++) {
+    const idPokemon = i + 1 + offset;
+    obtenerPokemon(idPokemon).then((respuestaPokemon) => {
+      const {
+        id,
+        name: nombrePokemon,
+        types: tipos,
+        sprites: {
+          other: {
+            'official-artwork': { front_default: urlSprite },
+          },
+        },
+      } = respuestaPokemon;
+
+      cargarDatosPokemon(
+        mayusculaEnPrimaLetra(nombrePokemon),
+        tipos.map((tipo) => tipo.type.name),
+        urlSprite,
+        id,
+        i,
+      );
+    });
+  }
 }
 
 function borrarPokemonesAnteriores() {
